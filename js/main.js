b4d3990e1446f54a8c7acd1bdb96fc20cf04c4a3
robbyae <robbyastrong@gmail.com>
2025-04-28 13:57:49 -0500

    lessons section

--- js/main.js
+++ js/main.js
@@ -1,15 +1,19 @@
 $(document).on('click', '.home-nav', function() {
   $('#active-page').html($('#home-container').html());
+  $('.home-nav').css('display','none');
 });
 
 $(document).on('click', '.proj-nav', function() {
   $('#active-page').html($('#projects-container').html());
+  $('.home-nav').css('display','inline');
 });
 
 $(document).on('click', '.music-nav', function() {
-  $('#active-page').html($('#music-container').html());
+  $('#active-page').html($('#lessons-container').html());
+  $('.home-nav').css('display','inline');
 });
 
 $(document).on('click', '.contact-nav', function() {
   $('#active-page').html($('#contact-container').html());
+  $('.home-nav').css('display','inline');
 });
